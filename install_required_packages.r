6059b32bd21932a7e75b3c21bf5c13b7356345b4
kasperwelbers <kasperwelbers@gmail.com>
2015-04-15 09:52:34 +0200

    added install_required_packages.r
    Former-commit-id: 0aad341ccfba315d79f202933aa68fe48093c2af

---
+++ install_required_packages.r
@@ -0,0 +1,7 @@
+install.packages('devtools')
+library(devtools)
+
+install_github('vanatteveldt/topicbrowser')
+install_github('amcat/amcat-r')
+install_github('kasperwelbers/corpus-tools')
+install_github('kasperwelbers/semnet')
